2a96e913a7869b4bbd873cfed6542d2e3189901b
Robert Szczepanski <robert.szczepanski@cmsrs.pl>
2020-09-01 11:42:12 +0200

    git ignore

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 //export const SERVER_URL = 'http://demo.cmsrs.pl';
-export const SERVER_URL = 'http://127.0.0.1:8000';
+export const SERVER_URL = 'http://dev.cmsrs.pl';
+//export const SERVER_URL = 'http://127.0.0.1:8000';
